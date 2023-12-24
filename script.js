15c93067f02509abbc3f46a85fbe2a034881b704
RandilT <randiltharusha72@gmail.com>
2023-12-24 20:33:37 +0530

    Refactor the animation logic

--- script.js
+++ script.js
@@ -8,20 +8,41 @@ const spriteWidth = 575; // width of sprite sheet / number of frames in a row
 const spriteHeight = 523; // height of sprite sheet / number of rows
 let frameX = 0; // column of sprite sheet
 let frameY = 0; // row of sprite sheet
-let frameColumns = 6; // number of frames in a row
+let frameColumns = 0; // number of frames in a row
 let gameFrames = 0;
 const staggerFrame = 5; // change to slow down animation ex: 5 = 5 frames per second
+
+spriteAnimations = [
+    // array of animations
+    "idle" ={
+        width: 525,
+        height: 523,
+        loc:[],
+    },
+      "jump" ={
+        width: 120,
+        height: 120,
+        loc:[],
+    },
+      "run" ={
+        width: 1200,
+        height: 1250,
+        loc:[],
+    },
+]
 
 const playerImg = new Image();
 playerImg.src = "shadow_dog.png";
 
 function drawPlayer() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT); // clear canvas to prevent overlapping images
-  //
-  //   ctx.drawImage(image, sourceX, sourceY, sourceWidth, sourceHeight, destinationX, destinationY, destinationWidth, destinationHeight)
+
+  let position = Math.floor(gameFrames / staggerFrame) % 6; // position of sprite sheet to draw
+  frameX = spriteWidth * position; // x coordinate of sprite sheet to start drawing from
+  // ctx.drawImage(image, sourceX, sourceY, sourceWidth, sourceHeight, destinationX, destinationY, destinationWidth, destinationHeight)
   ctx.drawImage(
     playerImg,
-    frameX * spriteWidth,
+    frameX,
     frameY * spriteHeight,
     spriteWidth,
     spriteHeight,
@@ -36,9 +57,7 @@ function drawPlayer() {
       frameX++; // move to next frame only if frameX is less than the number of frames in a row (frameColumns) and if gameFrames is divisible by staggerFrame
     else frameX = 0;
   }
-
-  requestAnimationFrame(drawPlayer); // loop animation
-
   gameFrames++; // increment gameFrames to keep track of how many frames have passed since the animation started (used to slow down animation) - see if statement above
+  requestAnimationFrame(drawPlayer); // loop animation
 }
 drawPlayer(); // call function to start animation
