d3e06b1b6dcf73001bed1edb16cbf7a0bab6cc35
RandilT <randiltharusha72@gmail.com>
2023-12-24 19:28:14 +0530

    cutout the sprite

--- script.js
+++ script.js
@@ -4,13 +4,27 @@ const ctx = canvas.getContext("2d");
 // Constants
 const CANVAS_WIDTH = (canvas.width = 600);
 const CANVAS_HEIGHT = (canvas.height = 600);
+const spriteWidth = 575;
+const spriteHeight = 523;
 
 const playerImg = new Image();
-playerImg.src = "images/shadow_dog.png";
+playerImg.src = "shadow_dog.png";
 
 function drawPlayer() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  ctx.fillRect(50, 50, 100, 100);
+  //
+  //   ctx.drawImage(image, sourceX, sourceY, sourceWidth, sourceHeight, destinationX, destinationY, destinationWidth, destinationHeight)
+  ctx.drawImage(
+    playerImg,
+    0,
+    0,
+    spriteWidth,
+    spriteHeight,
+    0,
+    0,
+    CANVAS_WIDTH,
+    CANVAS_HEIGHT
+  );
   requestAnimationFrame(drawPlayer);
 }
 drawPlayer();
